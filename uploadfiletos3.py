fd7de2c4e1b227329f106c2140daf3a13e9e8303
mahmoud-24 <mahmoud.ibrahim2411@gmail.com>
2023-01-16 02:28:48 +0200

    pushhh

--- uploadfiletos3.py
+++ uploadfiletos3.py
@@ -1,4 +1,4 @@
 import boto3
 s3 = boto3.resource('s3')
-s3.meta.client.upload_file('~/microserviese-task/', 's3-bucket-micro-task', '')
+s3.meta.client.upload_file('~/microserviese-task/test-file.txt/', 's3-bucket-micro-task', 'test-file.txt')
 print ("file uploaded successfully")
